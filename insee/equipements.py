4e034c8502e91f15746de05815dd0e35ddb3703a
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2016-01-22 15:21:32 +0100

    minor modif

--- insee/equipements.py
+++ insee/equipements.py
@@ -2,18 +2,18 @@
 """
 Created on Fri Dec  4 09:40:36 2015
 
-@author: User
 """
+from __future__ import print_function
+
 import pandas as pd
 from globals import path_insee, path_data, _read_file_or_download
-
 
 
 def equipement_url(filename, year):
     if year == 2013:
         year_str = str(year)[-2:]
     if year == 2014:
-        year_str = str(year)       
+        year_str = str(year)
     url_path = path_insee + filename + '/' + filename + '-infra-' + year_str + '.zip'
     return url_path
 
@@ -46,30 +46,30 @@ def sum_of_all_features(iris_df, colname, list_to_sum=None):
 columns_not_to_sum = ['CODGEO', 'LIBGEO', 'COM', 'LIBCOM', 'REG', 'DEP', 'ARR', 'CV',
              'ZE2010', 'UU2010', 'UU12010', 'ID_MODIF_GEO']
 
-# Useless
-#equipements = dict(
-#    commerce = ('equip-serv-commerce', None),
-#    sport = ('equip-sport-loisir-socio',
-#                      ['NB_F101', 'NB_F102', 'NB_F103', 'NB_F104',
-#                     'NB_F105', 'NB_F106', 'NB_F107', 'NB_F108',
-#                     'NB_F109', 'NB_F110', 'NB_F111', 'NB_F112',
-#                     'NB_F113', 'NB_F114', 'NB_F115', 'NB_F117', 'NB_F118']),
-#    enseignement_degre_1 = ('equip-serv-ens-1er-degre', ['NB_C101', 'NB_C102', 'NB_C104', 'NB_C105']),
-#    enseignement_degre_2 = ('equip-serv-ens-2eme-degre', ['NB_C201', 'NB_C301', 'NB_C302', 'NB_C303',
-#                              'NB_C304', 'NB_C305']),
-#    enseignement_sup = ('equip-serv-ens-sup-form-serv',
-#                                ['NB_C401', 'NB_C402', 'NB_C403',
-#                                'NB_C409', 'NB_C501', 'NB_C502',
-#                                'NB_C503', 'NB_C504', 'NB_C509',
-#                                'NB_C601', 'NB_C602', 'NB_C603',
-#                                'NB_C604', 'NB_C605', 'NB_C609',
-#                                'NB_C701', 'NB_C702']),
-#     social = ('equip-serv-action-sociale', None),
-#     sante = ('equip-serv-sante', None),
-#     medical = ('equip-serv-medical-para', None),
-#     service_particulier = ('equip-serv-particuliers', None),
-#     transport_tourisme = ('equip-tour-transp', None),
-     )
+# The list of variables is useless now
+equipements = dict(
+    commerce = ('equip-serv-commerce', None),
+    sport = ('equip-sport-loisir-socio',
+                      ['NB_F101', 'NB_F102', 'NB_F103', 'NB_F104',
+                     'NB_F105', 'NB_F106', 'NB_F107', 'NB_F108',
+                     'NB_F109', 'NB_F110', 'NB_F111', 'NB_F112',
+                     'NB_F113', 'NB_F114', 'NB_F115', 'NB_F117', 'NB_F118']),
+    enseignement_degre_1 = ('equip-serv-ens-1er-degre', ['NB_C101', 'NB_C102', 'NB_C104', 'NB_C105']),
+    enseignement_degre_2 = ('equip-serv-ens-2eme-degre', ['NB_C201', 'NB_C301', 'NB_C302', 'NB_C303',
+                              'NB_C304', 'NB_C305']),
+    enseignement_sup = ('equip-serv-ens-sup-form-serv',
+                                ['NB_C401', 'NB_C402', 'NB_C403',
+                                'NB_C409', 'NB_C501', 'NB_C502',
+                                'NB_C503', 'NB_C504', 'NB_C509',
+                                'NB_C601', 'NB_C602', 'NB_C603',
+                                'NB_C604', 'NB_C605', 'NB_C609',
+                                'NB_C701', 'NB_C702']),
+     social = ('equip-serv-action-sociale', None),
+     sante = ('equip-serv-sante', None),
+     medical = ('equip-serv-medical-para', None),
+     service_particulier = ('equip-serv-particuliers', None),
+     transport_tourisme = ('equip-tour-transp', None),
+    )
 
 
 def routine1(name, year):
@@ -95,7 +95,7 @@ def info_equipement(year):
             equipement = routine1(table, year)
         else:
             equipement = equipement.merge(routine1(table, year), how='outer')
-    
+
     sport = [x for x in equipement.columns if x[:5] == 'NB_F1' and len(x) == 7]
     airjeu_sport = [x for x in equipement.columns if x[:5] == 'NB_F1' and x[-10:] == 'NB_AIREJEU']
     nb_enseignement_1 = [x for x in equipement.columns if x[:5] == 'NB_C1'  and len(x) == 7]
