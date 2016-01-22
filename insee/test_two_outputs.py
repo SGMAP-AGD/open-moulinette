cada9ff7557c229e4ee7c08566c9961151d07ed4
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2016-01-22 18:09:09 +0100

    doublons...

--- insee/test_two_outputs.py
+++ insee/test_two_outputs.py
@@ -27,6 +27,15 @@ print('dans 2 et pas dans 1', in_2_not_in_1)
 tab1.drop(in_1_not_in_2, axis=1, inplace=True)
 tab2.drop(in_2_not_in_1, axis=1, inplace=True)
 
+
+# order tab2 as tab1 (may be useless)
+tab2 = tab2[tab1.columns]
+
+# differents types
+string_columns = tab1.dtypes[tab1.dtypes == 'object'].index.tolist()
+float_columns = [x for x in tab1.columns if x not in string_columns]
+
+
 # comparaison des types
 pb_dtypes = tab1.dtypes != tab2.dtypes
 
@@ -44,13 +53,18 @@ in_2_not_in_1, in_1_not_in_2 = _compare(tab1.CODGEO, tab2.CODGEO)
 # CODGEO => OK
 
 tab1.sort_values('CODGEO', inplace=True)
-
 tab2.sort_values('CODGEO', inplace=True)
 
-# order tab2 as tab1 (may be useless)
-tab2 = tab2[tab1.columns]
-string_columns = tab1.dtypes[tab1.dtypes == 'object'].index.tolist()
-float_columns = [x for x in tab1.columns if x not in string_columns]
+## en passant, il faut probablement un autre programme mais il nous faut vérifier pourquoi
+# on a des doublons de CODGEO, ie, pourquoi on n'a pas toujours les mêmes caractérisitques
+doublons = tab1['CODGEO'].value_counts() > 1
+doublons = doublons[doublons].index.tolist()
+# exemple
+tab1.loc[tab1['CODGEO'] == doublons[2], string_columns] # a chaque fois, on a une valeurs nulle
+tab1.loc[tab1['CODGEO'] == doublons[2], :].iloc[0,:]
+test = tab1.loc[tab1['CODGEO'] == doublons[2], :].iloc[1,:].notnull()
+
+
 
 # test sur les NaN
 diff = tab2[float_columns].isnull() == tab1[float_columns].isnull()
