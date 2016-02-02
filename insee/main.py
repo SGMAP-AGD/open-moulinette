e12b0ed518e356b8da4da529acbae53587352cbc
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2016-02-02 11:37:12 +0100

    changes in main

--- insee/main.py
+++ insee/main.py
@@ -16,11 +16,13 @@ from census import info_population
 
 # equipement
 equipement = info_equipement(2013)
+equipement['CODGEO'] = equipement['CODGEO'].astype(str).str.lstrip('0')
 # revenu
 revenu = info_revenus(2011)
 revenu['LIBCOM'] = revenu['LIBCOM'].str.replace(' - ', '-')
 # => on prend pour les villes à arrondissement, les valeurs population et equipement
 # juse parce qu'elles sont deux et que revenu est tout seul
+revenu['CODGEO'] = revenu['CODGEO'].astype(str).str.lstrip('0')
 
 del revenu['LIBCOM']; del revenu['COM'];
 # population
@@ -28,6 +30,7 @@ population = info_population(2011)
 population['LIBGEO'] = population['LIBGEO'].str.replace(u"Jean Bart Guynemer", u"Jean Bart,Guynemer")
 population['LIBGEO'] = population['LIBGEO'].str.replace(u"Nouveau Siècle", u"Nouveau Siecle")
 population['LIBGEO'] = population['LIBGEO'].str.replace(u"Labuissière", u"Labuissiere")
+population['CODGEO'] = population['CODGEO'].astype(str).str.lstrip('0')
 #population12 = info_population(2012)
 
 for table in [equipement, population]:
@@ -44,9 +47,28 @@ data = data.merge(population, how='outer')
 ## Ce cogeo n'est que dans revenu, on ajoute les valeur COM et LIBCOM que l'on a retiré
 data.loc[data['CODGEO'] == '893870113', ['LIBCOM','COM']] = [['Sens', '89387']]
 
-data.to_csv('data/output11_dev.csv', sep=';', index=False, encoding='utf-8')
 
-www
+population12 = info_population(2012)
+population12['LIBGEO'] = population12['LIBGEO'].str.replace(u"Jean Bart Guynemer", u"Jean Bart,Guynemer")
+population12['LIBGEO'] = population12['LIBGEO'].str.replace(u"Nouveau Siècle", u"Nouveau Siecle")
+population12['LIBGEO'] = population12['LIBGEO'].str.replace(u"Labuissière", u"Labuissiere")
+
+string_columns = population12.dtypes[population12.dtypes == 'object'].index.tolist()
+for col in string_columns:
+    population12[col] = population12[col].str.lstrip('0')
+data['MODIF_IRIS'] = data['MODIF_IRIS'].str.replace('00', '')
+
+string_columns = data.dtypes[data.dtypes == 'object'].index.tolist()
+for col in string_columns:
+    data[col] = data[col].str.lstrip('0')
+data['MODIF_IRIS'] = data['MODIF_IRIS'].str.replace('00', '')
+
+population12['LIBGEO'] = population12['LIBGEO'].str.title()
+
+
+data['GRD_QUART'] = data['GRD_QUART'].astype(str)
+population12['GRD_QUART'] = population12['GRD_QUART'].astype(str)
+population12['GRD_QUART']
 
 new_reg_dict = {'01' : '01',
                 '02' : '02',
@@ -75,6 +97,14 @@ new_reg_dict = {'01' : '01',
                 '93' : '93',
                 '94' : '94'}
 
-data['REG2016'] = data.REG.map(new_reg_dict)
 
-data.to_csv('data/output.csv')
+compare_geo(data, population12, debug=True)
+data2 = data.merge(population12, how='outer')
+
+data2['REG2016'] = data.REG.map(new_reg_dict)
+# recommandation: merge on CODGEO only and take 2012 LIBGEO.
+# LIBGEO12 seems an update of LIBGEO11
+# examples : Mendela => Mandela; Anne Franck => Anne Frank,...
+# pareil pour LIBCOM ?
+
+#data2.to_csv('data/output_dev12.csv', sep=';', index=False, encoding='utf-8')
